f9c81ee4dab035a74a2f56321e1a26d6c2205a9e
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-02 13:53:25 -0700

    all routes implemented, including NotFoundPage

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,18 +1,19 @@
-import './NavBar.css'
-import { NavLink } from 'react-router-dom';
+import "./NavBar.css";
+import { NavLink } from "react-router-dom";
 import logo from "../images/yogability-logo.png";
-
 
 const NavBar = () => {
   return (
     <>
-      <nav className='nav-container'>
-        <NavLink className="homepage-btn">Home</NavLink>
-          <img className='logo' src={logo} alt="yogability-logo" />
-        <NavLink className="saved-asanas-btn">Saved Asanas</NavLink>
+      <nav className="nav-container">
+        <NavLink to="/home" className="homepage-btn">
+          Home
+        </NavLink>
+        <img className="logo" src={logo} alt="yogability-logo" />
+        <NavLink to="/saved" className="saved-asanas-btn">Saved Asanas</NavLink>
       </nav>
     </>
   );
 };
 
-export default NavBar
+export default NavBar;
