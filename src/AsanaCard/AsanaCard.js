29e9c7c0013148b297f01d3f01d13570d73df527
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-02 13:22:32 -0700

    removed flexbox from asana cards & changed height

--- src/AsanaCard/AsanaCard.js
+++ src/AsanaCard/AsanaCard.js
@@ -4,7 +4,7 @@ const AsanaCard = ({ pose }) => {
   return (
     <div className="asana-card">
       <h3 className="name">{pose.english_name}</h3>
-      <p className="sanskrit-name">Sanskrit Name: {pose.sanskrit_name}</p>
+      <p className="sanskrit-name">{pose.sanskrit_name}</p>
       <img className="asana-image" src={pose.url_svg} alt={pose.english_name} />
       <p className="description">Description: {pose.pose_description}</p>
       <p className="benefits">Benefits: {pose.pose_benefits}</p>
