3333183dd19295931d8eb3236b7d6e1e2dbe5e46
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-02 11:24:41 -0700

    classNames added

--- src/AsanaCard/AsanaCard.js
+++ src/AsanaCard/AsanaCard.js
@@ -2,11 +2,11 @@
 const AsanaCard = ({ pose }) => {
   return (
     <div className="asana-card">
-      <h3>{pose.english_name}</h3>
-      <p>Sanskrit Name: {pose.sanskrit_name}</p>
-      <p>Description: {pose.pose_description}</p>
-      <p>Benefits: {pose.pose_benefits}</p>
-      <img src={pose.url_svg} alt={pose.english_name} />
+      <h3 className="name">{pose.english_name}</h3>
+      <p className="sanskrit-name">Sanskrit Name: {pose.sanskrit_name}</p>
+      <p className="description">Description: {pose.pose_description}</p>
+      <p className="benefits">Benefits: {pose.pose_benefits}</p>
+      <img className="asana-image" src={pose.url_svg} alt={pose.english_name} />
     </div>
   );
 };
