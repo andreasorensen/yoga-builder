8d40a9456b9f2212bbb17863c74604ee322c4518
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-02 18:44:58 -0700

    updated apiCalls error messaging

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,11 +3,11 @@ const getPoses = async (selectedLevel) => {
   const response = await fetch(`https://yoga-api-nzy4.onrender.com/v1/poses?level=${selectedLevel}`);
 
   if (!response.ok && response.status >= 500 && response.status < 600) {
-    throw new Error(`Server error: ${response.status}`)
+    throw new Error(`I'm sorry, something went wrong on our end. Please try again later. Server Error: ${response.status}`)
   }
 
-  if (!response.ok && response.status === 404) {
-    throw new Error(`Server error: ${response.status}`)
+  if (!response.ok && response.status >= 400 && response.status < 500) {
+    throw new Error(`I'm sorry, something went wrong. Error: ${response.status}`)
   }
 
   return response.json();
