0476d62de35ee74d5e84e35c411a8cff545da9d0
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-02 19:03:15 -0700

    added styling to NotFoundPage & completed NotFoundPage component

--- src/NotFound/NotFoundPage.js
+++ src/NotFound/NotFoundPage.js
@@ -1,9 +1,14 @@
-
+import './NotFoundPage.css'
 
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
+    <>
+      <h3 className="not-found-msg">
+        We're sorry, but that page cannot be found.
+      </h3>
+      <p className='check-address-msg'>Please check that the address/URL is correct.</p>
+    </>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
