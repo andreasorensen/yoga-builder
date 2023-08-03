a45c4b48d66c16a952dee33ea4a463e9b6e61233
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-02 20:26:52 -0700

    inital styling of Form completed

--- src/FilterForm/FilterForm.js
+++ src/FilterForm/FilterForm.js
@@ -5,9 +5,9 @@ const FilterForm = ({ selectedLevel, handleLevelChange, onSubmit }) => {
   return (
     <div className='form-container'>
     <form onSubmit={onSubmit} className='form'>
-      <h3 className='curr-level'>Please select your current experience level</h3>
-      <label className='form-label'>
-        <select value={selectedLevel} onChange={handleLevelChange}>
+      <h3 className='curr-level'>Begin exploring asanas by selecting your current experience level below.</h3>
+      <label>
+        <select className='drop-down' value={selectedLevel} onChange={handleLevelChange}>
           <option value="beginner">Beginner</option>
           <option value="intermediate">Intermediate</option>
           <option value="expert">Expert</option>
