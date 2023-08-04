41e825a502cf97ad9628e7b656366448bcd3456d
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-04 13:07:11 -0700

    testing for routes completed

--- cypress/e2e/Routes.cy.js
+++ cypress/e2e/Routes.cy.js
@@ -3,23 +3,20 @@ describe('App routing', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('visits the homepage', () => {
-    cy.url().should('include', '/')
-    // cy.get('.form-container')
-    // cy.contains('Please select a level before submitting.')
-  })
-
-  it('navigates to /saved when Saved Asanas link is clicked', () => {
+  it('should have different rountes when navigating with homepage button & saved buttons', () => {
     cy.get('.saved-asanas-btn').click() 
     cy.url().should('include', '/saved')
-    cy.get('.saved-poses-container') // assuming SavedPage has a class name of 'saved-poses-container'
-    cy.contains('You currently have no saved asanas.') // assuming this text appears by default on SavedPage
+    cy.get('.saved-poses-container')
+    cy.contains('You currently have no saved asanas.')
+
+    cy.get('.homepage-btn').click()
+    cy.url().should('include', '/')
   })
 
   it('displays NotFoundPage for non-existing routes', () => {
     cy.visit('http://localhost:3000/non-existing')
     cy.url().should('include', '/non-existing')
-    cy.get('.not-found-msg') // assuming NotFoundPage has a class name of 'not-found-msg'
+    cy.get('.not-found-msg')
     cy.contains('We\'re sorry, but that page cannot be found.') 
   })
 })
