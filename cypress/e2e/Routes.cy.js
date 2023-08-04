2c889034c63feb9ac4a59dea6c86c8711e880a32
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-04 12:57:04 -0700

    working on routes testing, incomplete

--- cypress/e2e/Routes.cy.js
+++ cypress/e2e/Routes.cy.js
@@ -1,9 +1,25 @@
-//Homepage
-//Saved Page
-//incorrect route
+describe('App routing', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('visits the homepage', () => {
+    cy.url().should('include', '/')
+    // cy.get('.form-container')
+    // cy.contains('Please select a level before submitting.')
+  })
+
+  it('navigates to /saved when Saved Asanas link is clicked', () => {
+    cy.get('.saved-asanas-btn').click() 
+    cy.url().should('include', '/saved')
+    cy.get('.saved-poses-container') // assuming SavedPage has a class name of 'saved-poses-container'
+    cy.contains('You currently have no saved asanas.') // assuming this text appears by default on SavedPage
+  })
+
+  it('displays NotFoundPage for non-existing routes', () => {
+    cy.visit('http://localhost:3000/non-existing')
+    cy.url().should('include', '/non-existing')
+    cy.get('.not-found-msg') // assuming NotFoundPage has a class name of 'not-found-msg'
+    cy.contains('We\'re sorry, but that page cannot be found.') 
   })
 })
--- src/FilterForm/FilterForm.js
+++ src/FilterForm/FilterForm.js
@@ -1,21 +1,30 @@
-import PropTypes from 'prop-types'
-import './FilterForm.css'
+import PropTypes from "prop-types";
+import "./FilterForm.css";
 
 const FilterForm = ({ selectedLevel, handleLevelChange, onSubmit }) => {
   return (
-    <div className='form-container'>
-    <form onSubmit={onSubmit} className='form'>
-      <h3 className='curr-level'>Begin exploring asanas by selecting your current experience level below.</h3>
-      <label>
-        <select className='drop-down' value={selectedLevel} onChange={handleLevelChange}>
-          <option value="">Select your level</option>
-          <option value="beginner">Beginner</option>
-          <option value="intermediate">Intermediate</option>
-          <option value="expert">Expert</option>
-        </select>
-      </label>
-      <button className='submit-btn' type="submit">Submit</button>
-    </form>
+    <div className="form-container">
+      <form onSubmit={onSubmit} className="form">
+        <h3 className="curr-level">
+          Begin exploring asanas by selecting your current experience level
+          below.
+        </h3>
+        <label>
+          <select
+            className="drop-down"
+            value={selectedLevel}
+            onChange={handleLevelChange}
+          >
+            <option value="">Select your level</option>
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="expert">Expert</option>
+          </select>
+        </label>
+        <button className="submit-btn" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
